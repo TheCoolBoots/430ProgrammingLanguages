b91e28ced1e1826220b20a81512713d16f47638e
TheCoolBoots <arjlai221@gmail.com>
2020-10-04 14:40:30 -0700

    Creating Personal Branch

--- Assignment 2.rkt
+++ Assignment 2.rkt
@@ -40,46 +40,48 @@
 
 |#
 
-(define-type ExprC (U numC binopC ifleq0C))
+(define-type ExprC (U numC binopC ifleq0C idC appC))
 (struct numC ([n : Real]) #:transparent)
 (struct ifleq0C ([expIf : ExprC] [expThen : ExprC] [expElse : ExprC]) #:transparent)
-(struct binopC ([op : OperatorC]) #:transparent)
+(struct binopC ([op : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
+(struct idC ([s : Symbol]))
+(struct appC ([fun : Symbol] [arg : ExprC]))
 
 
-(define-type OperatorC (U addC subC multC divC))
-(struct addC ([l : ExprC] [r : ExprC]) #:transparent)
-(struct subC ([l : ExprC] [r : ExprC]) #:transparent)
-(struct multC ([l : ExprC] [r : ExprC]) #:transparent)
-(struct divC ([l : ExprC] [r : ExprC]) #:transparent)
-
+(define-type FunDefC fdC)
+(struct fdC ([name : Symbol] [arg : Symbol] [body : ExprC]))
 
 ; interprets an ExprC into a real number
-(: interp (-> ExprC Real))
-(define (interp expr)
+(: interp (-> (Listof FunDefC) ExprC Real))
+(define (interp fds expr)
   (match expr
     [(numC n) n]
-    [(binopC op) (interpBinop op)]
-    [(ifleq0C expIf expThen expElse) (ifleq0 expIf expThen expElse)]))
+    [(binopC op l r) (interpBinop fds expr)]
+    [(ifleq0C expIf expThen expElse) (ifleq0 fds expIf expThen expElse)]))
 
 
 ; interprets a binary operator
-(: interpBinop (-> OperatorC Real))
-(define (interpBinop op)
+(: interpBinop (-> (Listof FunDefC) binopC Real))
+(define (interpBinop fds op)
   (match op
-    [(addC l r) (+ (interp l) (interp r))]
-    [(subC l r) (- (interp l) (interp r))]
-    [(multC l r) (* (interp l) (interp r))]
-    [(divC l r) (/ (interp l) (interp r))]))
+    [(binopC op l r)
+     (match op
+       ['+ (+ (interp fds l) (interp fds r))]
+       ['- (- (interp fds l) (interp fds r))]
+       ['/ (/ (interp fds l) (interp fds r))]
+       ['* (* (interp fds l) (interp fds r))]
+       [other (error 'ERROR "Binary Operator unimplemented")])]))
+       
 
 
 ; handles the logic for if less than or equal to zero
 ; if expIf is less than or equal to zero, return expThen
 ; else, return expElse
-(: ifleq0 (-> ExprC ExprC ExprC Real))
-(define (ifleq0 expIf expThen expElse)
+(: ifleq0 (-> (Listof FunDefC) ExprC ExprC ExprC Real))
+(define (ifleq0 fds expIf expThen expElse)
  (cond
-   [(<= (interp expIf) 0) (interp expThen)]
-   [else (interp expElse)]))
+   [(<= (interp fds expIf) 0) (interp fds expThen)]
+   [else (interp fds expElse)]))
 
 
 ; converts an S-expression into an ExprC
@@ -107,42 +109,43 @@
 (: parseBinary (-> Sexp binopC))
 (define (parseBinary exp)
   (match exp
-    [(list '+ l r) (binopC (addC (parse l) (parse r)))]
-    [(list '- l r) (binopC (subC (parse l) (parse r)))]
-    [(list '* l r) (binopC (multC (parse l) (parse r)))]
-    [(list '/ l r) (binopC (divC (parse l) (parse r)))]))
+    [(list '+ l r) (binopC '+ (parse l) (parse r))]
+    [(list '- l r) (binopC '- (parse l) (parse r))]
+    [(list '* l r) (binopC '* (parse l) (parse r))]
+    [(list '/ l r) (binopC '/ (parse l) (parse r))]
+    [other (error 'ERROR "Binary Operator unimplemented")]))
 
-(check-equal? (parseBinary '(+ 1 2)) (binopC (addC (numC 1) (numC 2))))
+(check-equal? (parseBinary '(+ 1 2)) (binopC '+ (numC 1) (numC 2)))
 (check-equal? (parseBinary '(+ (- 1 2) 3))
-              (binopC (addC (binopC (subC (numC 1) (numC 2))) (numC 3))))
+              (binopC '+ (binopC '- (numC 1) (numC 2)) (numC 3)))
 (check-equal? (parseBinary '(/ 3 (* 1 3)))
-              (binopC (divC (numC 3) (binopC (multC (numC 1) (numC 3))))))
+              (binopC '/ (numC 3) (binopC '* (numC 1) (numC 3))))
 
 ; parses then interprets an S-expression
-(: top-interp (-> Sexp Number))
-(define (top-interp exp)
-  (interp (parse exp)))
+(: top-interp (-> (Listof FunDefC) Sexp Number))
+(define (top-interp fds exp)
+  (interp fds (parse exp)))
 
-(check-equal? (top-interp '(ifleq0 (+ 1 2) 0 2)) 2)
+;(check-equal? (top-interp '(ifleq0 (+ 1 2) 0 2)) 2)
 
 ; interpreting binary operators
-(check-equal? (interpBinop (addC (numC 1) (numC 2))) 3)
-(check-equal? (interpBinop (subC (numC 1) (numC 2))) -1)
-(check-equal? (interpBinop (multC (numC 1) (numC 2))) 2)
-(check-equal? (interpBinop (divC (numC 1) (numC 2))) 1/2)
-(check-equal? (interpBinop (addC (binopC (subC (numC 3) (numC 4))) (numC 2))) 1)
+;(check-equal? (interpBinop (binopC '+ (numC 1) (numC 2))) 3)
+;(check-equal? (interpBinop (binopC '- (numC 1) (numC 2))) -1)
+;(check-equal? (interpBinop (binopC '* (numC 1) (numC 2))) 2)
+;(check-equal? (interpBinop (binopC '/ (numC 1) (numC 2))) 1/2)
+;(check-equal? (interpBinop (binopC '+ (binopC '- (numC 3) (numC 4)) (numC 2))) 1)
 
 ; interpreting ExprC's
-(check-equal? (interp (binopC (addC (numC 3) (numC 2)))) 5)
-(check-equal? (interp (numC 5)) 5)
+;(check-equal? (interp (binopC '+ (numC 3) (numC 2))) 5)
+;(check-equal? (interp (numC 5)) 5)
 
 ; top-interp tests
-(check-equal? (top-interp '(ifleq0 (+ 1 2) 0 2)) 2)
-(check-equal? (top-interp '(ifleq0 (+ 0 0) 0 2)) 0)
+;(check-equal? (top-interp '(ifleq0 (+ 1 2) 0 2)) 2)
+;(check-equal? (top-interp '(ifleq0 (+ 0 0) 0 2)) 0)
 
 ; parsing a binary operator
-(check-equal? (opSymbol? '+) #t)
-(check-equal? (opSymbol? '-) #t)
-(check-equal? (opSymbol? '*) #t)
-(check-equal? (opSymbol? '/) #t)
-(check-equal? (opSymbol? 'k) #f)
+;(check-equal? (opSymbol? '+) #t)
+;(check-equal? (opSymbol? '-) #t)
+;(check-equal? (opSymbol? '*) #t)
+;(check-equal? (opSymbol? '/) #t)
+;(check-equal? (opSymbol? 'k) #f)
