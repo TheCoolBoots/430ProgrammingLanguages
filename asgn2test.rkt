d5214b25df867d9adc2878f4515d678a192c39d9
Ryan Shintaku <ryanshintaku@ryans-mbp.lan>
2020-10-04 18:16:41 -0700

    asgn2test.rkt passes all tests, needs cleaning

--- asgn2test.rkt
+++ asgn2test.rkt
@@ -11,37 +11,65 @@
 (struct id ([s : Symbol]) #:transparent)
 (struct app ([func : Symbol] [arg : DXUQ2]) #:transparent)
 
+
 ;;takes in s-expr and parses to create DXUQ2
 (define (parse [s : Sexp]) : DXUQ2
   (match s
     [(? real? r) (num s)]
-    [(list (? symbol? sym) l r) (binop sym (parse l) (parse r))]
-    [(list test then else) (ifleq0 (parse test) (parse then) (parse else))]
-    [(? symbol? sym) (id sym)]
+    [(list '+ l r) (binop '+ (parse l) (parse r))]
+    [(list '- l r) (binop '- (parse l) (parse r))]
+    [(list '* l r) (binop '* (parse l) (parse r))]
+    [(list '/ l r) (binop '/ (parse l) (parse r))]
+    [(list 'ifleq0 test then else) (ifleq0 (parse test) (parse then) (parse else))]
+    [(? symbol? sym) (match sym
+                       ['+  (error "invalid format DXUQ")]
+                       ['-  (error "invalid format DXUQ")]
+                       ['*  (error "invalid format DXUQ")]
+                       ['/  (error "invalid format DXUQ")]
+                       ['undef  (error "invalid format DXUQ")]
+                       ['ifleq0  (error "invalid format DXUQ")]
+                       ['fundef (error "invalid format DXUQ")]
+                       [other (id sym)])]
     [(list (? symbol? sym) arg) (app sym (parse arg))]
-    [other (error "invalid format")]))
+    [other (error "invalid format DXUQ")]))
 
 
 (check-equal? (parse 1) (num 1))
 (check-equal? (parse '(+ 1 2)) (binop '+ (num 1) (num 2)))
+(check-equal? (parse '(- 1 2)) (binop '- (num 1) (num 2)))
 (check-equal? (parse '(+ (+ 1 2) 3)) (binop '+ (binop '+ (num 1) (num 2)) (num 3)))
 (check-equal? (parse '(* (* 1 2) 3)) (binop '* (binop '* (num 1) (num 2)) (num 3)))
-(check-equal? (parse '(1 2 3)) (ifleq0 (num 1) (num 2) (num 3)))
-(check-equal? (parse '((+ 1 2) 2 3)) (ifleq0 (binop '+ (num 1) (num 2)) (num 2) (num 3)))
+(check-equal? (parse '(ifleq0 1 2 3)) (ifleq0 (num 1) (num 2) (num 3)))
+(check-equal? (parse '(ifleq0 (+ 1 2) 2 3)) (ifleq0 (binop '+ (num 1) (num 2)) (num 2) (num 3)))
 (check-equal? (parse 'x) (id 'x))
 (check-equal? (parse '(addone 1)) (app 'addone (num 1)))
-(check-exn (regexp (regexp-quote "invalid format"))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
            (lambda () (parse '(1 2 3 4 5))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ / 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ + 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ - 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ undef 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ ifleq0 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ * 3))))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (parse '(+ fundef 3))))
 
 ;;takes in s-expr and parses to make function
 (define (parse-fundef [s : Sexp]) : fundef
   (match s
     [(list 'fundef (list (? symbol? name) (? symbol? arg)) body) (fundef name arg (parse body))]
-    [other (error "invalid format")]))
+    ;[(list 'fundef (list (? symbol? name) (? symbol? arg)) (? list? l)) (fundef name arg (parse l))] 
+    [other (error "invalid format DXUQ")]))
 
 
 (check-equal? (parse-fundef '{fundef {addone x} {+ x 1}}) (fundef 'addone 'x (binop '+ (id 'x) (num 1))))
-(check-exn (regexp (regexp-quote "invalid format"))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
            (lambda () (parse-fundef '(1 x (+ x 1)))))
 
 
@@ -49,11 +77,11 @@
 ;;takes in s-expr and parses to make list of functions
 (define (parse-prog [s : Sexp]) : (Listof fundef)
   (match s
-    [(list) (error "invalid format")]
+    [(list) (error "invalid format DXUQ")]
     [(list a) (cons (parse-fundef a) '())]
     [(list a b) (cons (parse-fundef a) (cons (parse-fundef b) '()))]
     [(? list? l) (cons (parse-fundef (first l)) (parse-prog (rest l)))]
-    [other (error "invalid format")]))
+    [other (error "invalid format DXUQ")]))
 
 (check-equal? (parse-prog '((fundef (f x) (+ x 14))))
               (list (fundef 'f 'x (binop '+ (id 'x) (num 14)))))
@@ -64,43 +92,66 @@
               (list (fundef 'f 'x (binop '+ (id 'x) (num 14)))
                     (fundef 'f2 'z (binop '* (num 2) (num 3)))
                     (fundef 'f3 'y (binop '/ (id 'y) (num 1)))))
-(check-exn (regexp (regexp-quote "invalid format"))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
            (lambda () (parse-prog '())))
-(check-exn (regexp (regexp-quote "invalid format"))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
            (lambda () (parse-prog 'a)))
 
 
 
 ;;determine what bin op to perform given symbol and two DXUQ2
-(define (getBinOp [op : Symbol] [l : DXUQ2] [r : DXUQ2]) : Real
+(define (getBinOp [op : Symbol] [l : DXUQ2] [r : DXUQ2] [funcs : (Listof fundef)]) : Real
   (match op
-    ['+ (+ (interp l) (interp r))]
-    ['* (* (interp l) (interp r))]
-    ['- (- (interp l) (interp r))]
-    ['/ (/ (interp l) (interp r))]
-    [other (error "invalid format")]))
+    ['+ (+ (interpBinOps l funcs) (interpBinOps r funcs))]
+    ['* (* (interpBinOps l funcs) (interpBinOps r funcs))]
+    ['- (- (interpBinOps l funcs) (interpBinOps r funcs))]
+    ['/ (/ (interpBinOps l funcs) (interpBinOps r funcs))]
+    [other (error "invalid format DXUQ")]))
 
-(check-exn (regexp (regexp-quote "invalid format"))
-           (lambda () (getBinOp '^ (num 1) (num 2))))
+
+
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (getBinOp '^ (num 1) (num 2) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14)))))))
 
 ;;interpret DXUQ2 and outputs a real
-;(define (interp [dx : DXUQ2]) : Real
-;  (match dx
-;    [(num n) n]
-;    [(binop op l r) (getBinOp op l r)]
-;    [(ifleq0 test then else) (if (< (interp test) 1) (interp then) (interp else))]))
+(define (interpBinOps [dx : DXUQ2] [funcs : (Listof fundef)]) : Real
+  (match dx
+    [(num n) n]
+    [(app func arg) (define fd (get-fundef func funcs))
+                    (interpBinOps (subst arg (fundef-arg fd) (fundef-body fd)) funcs)]
+    [(binop op l r) (getBinOp op l r funcs)]
+    [(ifleq0 test then else) (if (< (interpBinOps test funcs) 1) (interpBinOps then funcs) (interpBinOps else funcs))]))
 
-(check-equal? (interp (num 5)) 5)
-(check-equal? (interp (binop '+ (num 2) (num 3))) 5)
-(check-equal? (interp (binop '* (num 5) (num 10))) 50)
-(check-equal? (interp (binop '- (num 23) (num 11))) 12)
-(check-equal? (interp (binop '/ (num 10) (num 5))) 2)
-(check-equal? (interp (ifleq0 (num 1) (num 2) (num 3))) 3)
-(check-equal? (interp (ifleq0 (num -1) (num 2) (num 3))) 2)
 
+
+(check-equal? (interpBinOps (num 5) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 5)
+(check-equal? (interpBinOps (binop '+ (num 2) (num 3)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 5)
+(check-equal? (interpBinOps (binop '* (num 5) (num 10)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 50)
+(check-equal? (interpBinOps (binop '- (num 23) (num 11)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 12)
+(check-equal? (interpBinOps (binop '/ (num 10) (num 5)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 2)
+(check-equal? (interpBinOps (ifleq0 (num 1) (num 2) (num 3)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 3)
+(check-equal? (interpBinOps (ifleq0 (num -1) (num 2) (num 3)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 2)
+
+
+
+(check-equal? (getBinOp '- (num 1) (num 0) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 1)
+(check-equal? (getBinOp '/ (num 0) (num 1) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 0)
+
+
+;;get the function definition that matches the symbol
 (define (get-fundef [sym : Symbol] [funs : (Listof fundef)]) : fundef
   (cond
-    [(empty? funs) (error "invalid format")]
+    [(empty? funs) (error "invalid format DXUQ")]
     [(cons? funs) (cond
                     [(equal? sym (fundef-name (first funs))) (first funs)]
                     [else (get-fundef sym (rest funs))])]))
@@ -111,11 +162,11 @@
 (check-equal? (get-fundef 'f (list (fundef 'f 'x (binop '+ (id 'x) (num 14)))
                     (fundef 'f2 'z (binop '* (num 2) (num 3)))))
               (fundef 'f 'x (binop '+ (id 'x) (num 14))))
-(check-exn (regexp (regexp-quote "invalid format"))
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
            (lambda () (get-fundef 'x '())))
 
 
-
+;;substitute the value into the functions
 (define (subst [what : DXUQ2] [for : Symbol] [in : DXUQ2]) : DXUQ2
   (match in
     [(num n) in]
@@ -136,31 +187,69 @@
 (check-equal? (subst (num 3) 'x (app 'f (id 'x))) (app 'f (num 3)))
 
 
+(define (is_main? [fn : Any]) : Boolean
+  (match fn
+    [(fundef 'main 'init body) #t]
+    [other #f]))
+
+(check-equal? (is_main? (fundef 'main 'init (binop '+ (num 1) (num 2)))) true)
+(check-equal? (is_main? (fundef 'f 'x (binop '+ (id 'x) (num 1)))) false)
+
+
+(define (not_main? [fn : Any]) : Boolean
+  (match fn
+    [(fundef 'main 'init body) #f]
+    [other #t]))
+
+(check-equal? (not_main? (fundef 'main 'init (binop '+ (num 1) (num 2)))) false)
+(check-equal? (not_main? (fundef 'f 'x (binop '+ (id 'x) (num 1)))) true)
+
 
 (define (interp-fns [funs : (Listof fundef)]) : Real
-  
-    
+   (define main (filter is_main? funs))
+   (define notMain(filter not_main? funs))
+  (if (= (length main) 0) (error "invalid format DXUQ")
+      (interp (subst (num 0) 'init (fundef-body (first main))) notMain)))
+  ;(interp2 (subst (num 0) 'init (fundef-body (first main))) notMain))
 
-    
 
    
-(define (interp2 [exp : DXUQ2] [funs : (Listof fundef)]) : Real
+(define (interp [exp : DXUQ2] [funs : (Listof fundef)]) : Real
   (match exp
     [(num n) n]
-    [(binop op l r) (getBinOp op l r)]
-    [(ifleq0 test then else) (if (< (interp2 test funs) 1) (interp2 then funs) (interp2 else funs))]
-    [(app func arg) (define fd (get-fundef func funs)) (interp2 (subst arg
+    [(binop op l r) (getBinOp op l r funs)]
+    [(ifleq0 test then else) (if (< (interp test funs) 1) (interp then funs) (interp else funs))]
+    [(app func arg) (define fd (get-fundef func funs)) (interp (subst arg
                                                                        (fundef-arg fd)
                                                                        (fundef-body fd))
                                                                 funs)]
-    [(id sym) (error "invalid format")]))
+    [(id sym) (error "invalid format DXUQ")]))
+
+(check-equal? (interp-fns (parse-prog '{{fundef {f x} {+ x 14}}
+                     {fundef {main init} {f 2}}})) 16)
+(check-equal? (interp-fns (parse-prog '{{fundef {f x} {+ x 14}}
+                     {fundef {main init} {+ 1 2}}})) 3)
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (interp-fns (parse-prog '{{fundef {f x} {+ x 14}}}))))
+
+(check-equal? (interp (num 1) (parse-prog '{{fundef {f x} {+ x 14}}})) 1)
+(check-equal? (interp (binop '* (num 2) (num 3)) (parse-prog  '{{fundef {f x} {+ x 14}}})) 6)
+(check-equal? (interp (ifleq0 (num 0) (num 1) (num 2)) (parse-prog  '{{fundef {f x} {+ x 14}}})) 1)
+(check-equal? (interp (ifleq0 (num 1) (num 1) (num 2)) (parse-prog  '{{fundef {f x} {+ x 14}}})) 2)
+(check-exn (regexp (regexp-quote "invalid format DXUQ"))
+           (lambda () (interp (id 'a) (parse-prog '{{fundef {f x} {+ x 14}}}))))
 
 (: top-interp (Sexp -> Real))
 (define (top-interp fun-sexps)
   (interp-fns (parse-prog fun-sexps)))
 
+(check-equal? (top-interp '{{fundef {f x} {+ x 14}}
+                     {fundef {main init} {f 2}}}) 16)
     
 ;(define (top-interp [s : Sexp])
 ;  (interp (parse s)))
+
+(check-equal? (interpBinOps (app 'f (num 2)) (list
+ (fundef 'f 'x (binop '+ (id 'x) (num 14))))) 16)
 
 
