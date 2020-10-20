940ee5a68ab8b2eda33f56169db8bf519d1d3076
David <dreko@calpoly.edu>
2020-10-20 13:03:16 -0700

    work on lab5

--- lab5.rkt
+++ lab5.rkt
@@ -4,7 +4,8 @@
 
 ;; 1 Warmup
 ;; accepts a function and an argument, applies function to argument
-(define one (lambda (f) (lambda (x) (f x))))
+(define one (lambda (f)
+              (lambda (x) (f x))))
 
 (check-= ((one length) '(1 2 3)) 3 0)
 (check-= ((one (lambda (x) 1234)) 10) 1234 0)
@@ -12,24 +13,39 @@
 ;; 2 Continuing on:
 ;; 1.
 ;; accepts a function and an argument, applies function to function on argument
-(define two (lambda (f) (lambda (x) (f (f x)))))
+(define two (lambda (f)
+              (lambda (x) (f (f x)))))
 
 (check-equal? ((two reverse) '(1 2 3)) '(1 2 3))
 (check-= ((two (lambda (x) (* x 2))) 2) 8 0)
 
 ;; 2.
 ;; accepts a function and an argument, returns the argument
-(define zero (lambda (f) (lambda (x) x)))
+(define zero (lambda (f)
+               (lambda (x) x)))
 
 (check-equal? ((zero reverse) '(1 2 3 4)) '(1 2 3 4))
 (check-equal? ((zero first) '(d a v)) '(d a v))
 
 ;; 3.
 ;; applies given function additional time on itself
-(define add1 (lambda (f) (lambda (x) (one (f x) x)))) 
+(define add1 (lambda (nlf)
+               (lambda (f)
+                 (lambda (x) ((nlf f) (f x))))))
+
+(check-equal? (((add1 one) reverse) '(1 2 3)) '(1 2 3))
+(check-equal? (((add1 two) reverse) '(1 2 3)) '(3 2 1))
 
 ;; 4.
-;;
+;; applies first arg to second arg "sum" of args amount of times
+(define add (lambda (nlf1)
+              (lambda (nlf2)
+                (lambda (f)
+                  (lambda (x) ((nlf1 f) ((nlf2 f) x)))))))
+
+(check-equal? ((((add two) two) reverse) '(1 2 3)) '(1 2 3))
+(check-equal? ((((add two) one) reverse) '(1 2 3)) '(3 2 1))
+(check-equal? ((((add one) two) reverse) '(1 2 3)) '(3 2 1))
 
 ;; 5.
 ;; accepts two arguments and returns the first one
@@ -53,4 +69,27 @@
 (check-equal? (((if fals) 10) "david") "david")
 
 ;; 8.
-;;
+(module sim-DXUQ4 racket
+    (provide
+     (rename-out [lambda fn]
+                 [my-let let])
+     #%module-begin
+     #%datum
+     #%app
+     + - * / = equal? <=
+     if)
+
+;; rewrite one in DXUQ4
+'{fn {f} {fn {x} {f x}}}
+
+;; rewrite two in DXUQ4
+'{fn {f} {fn {x} {f {f x}}}}  
+
+;; rewrite add in DXUQ4
+'{fn {nlf1} {fn {nlf2} {fn {f} {fn {x} {{nlf1 f} {{nlf2 f} x}}}}}}
+  
+  (define-syntax my-let
+    (syntax-rules (in =)
+      [(my-let [v = e] ... in eb)
+       ((lambda (v ...) eb) e ...)])))
+
