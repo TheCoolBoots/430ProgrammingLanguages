6570b200fc283e3bf9fcbd82765dcc4997f2fc61
David <dreko@calpoly.edu>
2020-10-20 10:15:40 -0700

    lab5 work

--- lab5.rkt
+++ lab5.rkt
@@ -12,36 +12,45 @@
 ;; 2 Continuing on:
 ;; 1.
 ;; accepts a function and an argument, applies function to function on argument
-(define two (lambda (f x) (f (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
 
-(check-equal? (two reverse '(1 2 3)) '(1 2 3))
-(check-= (two (lambda (x) (* x 2)) 2) 8 0)
+(check-equal? ((two reverse) '(1 2 3)) '(1 2 3))
+(check-= ((two (lambda (x) (* x 2))) 2) 8 0)
 
 ;; 2.
 ;; accepts a function and an argument, returns the argument
-(define zero (lambda (f x) x))
+(define zero (lambda (f) (lambda (x) x)))
 
-(check-equal? (zero reverse '(1 2 3 4)) '(1 2 3 4))
-(check-equal? (zero first '(d a v)) '(d a v))
+(check-equal? ((zero reverse) '(1 2 3 4)) '(1 2 3 4))
+(check-equal? ((zero first) '(d a v)) '(d a v))
 
 ;; 3.
 ;; applies given function additional time on itself
-(define add1 (lambda (f) (f f)))
+(define add1 (lambda (f) (lambda (x) (one (f x) x)))) 
 
 ;; 4.
 ;;
 
 ;; 5.
 ;; accepts two arguments and returns the first one
-(define tru (lambda (a b) a))
+(define tru (lambda (a) (lambda (b) a)))
 
-(check-equal? (tru "david" 123) "david")
-(check-equal? (tru "reko" "david") "reko")
+(check-equal? ((tru "david") 123) "david")
+(check-equal? ((tru "reko") "david") "reko")
 
 ;; 6.
 ;; accepts two arguments and returns the second one
-(define fals (lambda (a b) b))
+(define fals (lambda (a) (lambda (b) b)))
 
-(check-= (fals "david" 123) 123 0)
-(check-equal? (fals "david" "reko") "reko")
+(check-= ((fals "david") 123) 123 0)
+(check-equal? ((fals "david") "reko") "reko")
 
+;; 7.
+;;
+(define if (lambda (a) (lambda (b) (lambda (c) ((a b) c)))))
+
+(check-= (((if tru) 5) 4) 5 0)
+(check-equal? (((if fals) 10) "david") "david")
+
+;; 8.
+;;
