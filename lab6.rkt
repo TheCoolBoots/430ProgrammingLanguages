4a6d4153a5a30b2f54af3060f4346871b2447727
David <dreko@calpoly.edu>
2020-10-27 14:34:11 -0700

    finished lab6

--- lab6.rkt
+++ lab6.rkt
@@ -38,12 +38,22 @@
     [(NumC n) n]
     [(StrC n) n]
     [(IdC n) n]
-    [(AppC target args) (cons (unparse target) (cons (map (lambda (a) (unparse a)) args) '()))] 
+    [(AppC target args) (cons (unparse target) (map (lambda (a) (unparse a)) args))] 
     [(CondC if then else) (cons 'if (cons (unparse if) (cons (unparse then) (cons (unparse else) '()))))]
     [(LamC ids body) (cons 'fn (cons (map (lambda (a) (unparse a)) ids) (cons (unparse body) '())))]
-    [else 'work]))
+    ))
 
 (check-equal? (unparse (NumC 1000)) 1000)
 (check-equal? (unparse (StrC "david")) "david")
 (check-equal? (unparse (IdC 's)) 's)
 (check-equal? (unparse (AppC (IdC 's) (list (IdC 'a) (IdC 'c)))) '(s a c))
+(check-equal? (unparse (CondC (IdC 'd) (IdC 's) (IdC 'a))) '(if d s a))
+(check-equal? (unparse (LamC (list (IdC 's) (IdC 's1)) (NumC 10))) '(fn (s s1) 10))
+
+;; quiz
+(define (quiz) : ExprC
+  (define expr (random-term (random 0 3)))
+  (printf (~v (unparse expr)))
+  expr)
+
+(define secret (quiz))
