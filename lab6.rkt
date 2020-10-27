fe58f19a5ae519a3254411bb14cb14b8f339efac
David <dreko@calpoly.edu>
2020-10-27 14:18:16 -0700

    work on lab6

---
+++ lab6.rkt
@@ -0,0 +1,49 @@
+#lang typed/racket
+
+(require typed/rackunit)
+
+; definitions for ExprC types
+(define-type ExprC (U IdC AppC CondC LamC NumC StrC))
+(struct NumC ([n : Real]) #:transparent)
+(struct IdC ([s : Symbol]) #:transparent)
+(struct StrC ([s : String]) #:transparent)
+(struct AppC ([target : ExprC] [args : (Listof ExprC)]) #:transparent)
+(struct CondC ([if : ExprC] [then : ExprC] [else : ExprC]) #:transparent)
+(struct LamC ([ids : (Listof ExprC)] [body : ExprC]) #:transparent)
+
+(define fixed-set (list 'a 'b 'c 'd 'e 'f 'g 'h))
+;; returns a random symbol from a fixed set of symbols
+(define (random-symbol) : Symbol
+  (list-ref fixed-set (random 0 8)))
+
+;; returns a random non-self-referential expression
+(define (random-base-term) : ExprC
+  (match (random 0 3)
+    [0 (NumC (random 0 1000))]
+    [1 (StrC (~v (random 0 1000)))]
+    [2 (IdC (random-symbol))]))
+
+;; returns a random expression
+(define (random-term [depth : Real]) : ExprC
+  (cond
+    [(= depth 0) (random-base-term)]
+    [else (match (random 0 3)
+            [0 (AppC (random-term (- depth 1)) (list (random-term(random 0 3))))]
+            [1 (CondC (random-term (- depth 1)) (random-term (- depth 1)) (random-term (- depth 1)))]
+            [2 (LamC (list (random-term(random 0 3))) (random-term (- depth 1)))])]))
+
+;; takes in a parsed expression and returns the concrete syntax
+(define (unparse [exp : ExprC]) : Sexp
+  (match exp
+    [(NumC n) n]
+    [(StrC n) n]
+    [(IdC n) n]
+    [(AppC target args) (cons (unparse target) (cons (map (lambda (a) (unparse a)) args) '()))] 
+    [(CondC if then else) (cons 'if (cons (unparse if) (cons (unparse then) (cons (unparse else) '()))))]
+    [(LamC ids body) (cons 'fn (cons (map (lambda (a) (unparse a)) ids) (cons (unparse body) '())))]
+    [else 'work]))
+
+(check-equal? (unparse (NumC 1000)) 1000)
+(check-equal? (unparse (StrC "david")) "david")
+(check-equal? (unparse (IdC 's)) 's)
+(check-equal? (unparse (AppC (IdC 's) (list (IdC 'a) (IdC 'c)))) '(s a c))
